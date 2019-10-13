9a2dfe22c840a6ec6701f9f01217650cbf84b7b7
JohnRaven <John-B-RAven@yandex.ru>
2019-10-13 23:07:56 +0300

    feat: add function (clean copy)

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.lowest = min;
+        this.highest = max;
     }
 
     guess() {
-
+        return Math.ceil((this.lowest+this.highest)/2);
     }
 
     lower() {
-
+        this.highest = this.guess();
     }
 
     greater() {
-
+        this.lowest =this.guess();
     }
 }
 
 module.exports = GuessingGame;
+
+
